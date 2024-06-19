cb8ed97909d3f34e9f95eef4cc2718d0a520e9c4
Abdul Azim <lokmanjamal7362@gmail.com>
2024-06-20 00:48:24 +0800

    commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>Group 2 All Rights Reserved |  <a href="#">GROUP 2</a> </p>
+</div>	
---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+
+
+
+
+<!--- /footer-top ---->
+<!---copy-right ---->
+<div class="copy-right">
+	<div class="container">
+		<p class="wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;"> Group 2 All Rights Reserved </p>
+	</div>
+</div>
